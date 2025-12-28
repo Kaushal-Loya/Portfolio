9bb65e27543e31b7c3e8fd5d5844381ed40b4852
Kaushal-Loya <kaushalloya5@gmail.com>
2025-12-28 23:51:10 +0530

    Final touchup

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,26 +11,9 @@ const spaceGrotesk = Space_Grotesk({
 })
 
 export const metadata: Metadata = {
-  title: "Portfolio | Software & AI Engineer",
+  title: "Kaushal Loya",
   description: "Computer Science & Engineering student specializing in scalable software and intelligent systems",
-  generator: "v0.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
+
 }
 
 export default function RootLayout({
